067a3e6fdf60535ae3ef17c93cbbd9216f4a3876
Alexander_Baranov <Sti_ll@mail.ru>
2025-12-03 19:47:03 +0300

    feat: add urls

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,13 @@
+from rest_framework.routers import DefaultRouter
+
 from app.apps import AppConfig
+from app.views import PayoutRequestViewSet
 
 app_name = AppConfig.name
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(prefix=r"payouts", viewset=PayoutRequestViewSet, basename='payouts')
+
+urlpatterns = [
+
+] + router.urls
