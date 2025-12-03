8b15a75e4c2d3256fe0cbe9a6e4ab2ce0af7078f
Alexander_Baranov <Sti_ll@mail.ru>
2025-12-03 19:46:54 +0300

    feat: add views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from app.models import PayoutRequest
+from app.serializers import PayoutRequestSerializer, PayoutRequestUpdateSerializer
+from app.tasks import process_payout_request
+
+
+class PayoutRequestViewSet(ModelViewSet):
+    """Реализация представления заявки на выплату через ViewSet (полный crud)"""
+    queryset = PayoutRequest.objects.all()
+    permission_classes = (AllowAny,)
+
+    def get_serializer_class(self):
+        if self.action == 'partial_update':
+            return PayoutRequestUpdateSerializer
+        return PayoutRequestSerializer
+
+    def perform_create(self, serializer):
+        payout = serializer.save()
+
+        # Запускаем асинхронную задачу
+        process_payout_request.delay(payout.id)
